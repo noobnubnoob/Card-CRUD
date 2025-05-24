5f5ecb923f24431f6a40b6620ce495709f8bd588
unknown <minvinkovic@gmail.com>
2025-05-24 15:29:02 +0200

    Get reqest for card req by oib

--- card.crud/src/main/java/com/ben/card/crud/controller/NewCreditCardRequestController.java
+++ card.crud/src/main/java/com/ben/card/crud/controller/NewCreditCardRequestController.java
@@ -1,9 +1,31 @@
 package com.ben.card.crud.controller;
 
+import com.ben.card.crud.dto.NewCardRequest;
+import com.ben.card.crud.model.NewCreditCardRequest;
+import com.ben.card.crud.service.NewCreditCardRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
 public class NewCreditCardRequestController {
+
+    @Autowired
+    NewCreditCardRequestService cardRequestService;
+
+    @RequestMapping(value = "/client-cards/{oib}", method = RequestMethod.GET)
+    public List<NewCardRequest> clientCardInformation(@PathVariable String oib) {
+
+        List<NewCreditCardRequest> creditCardRequests = cardRequestService.findByOib(oib);
+        List<NewCardRequest> cardRequests = creditCardRequests.stream().map(
+                x -> new NewCardRequest(x.getFirstName(), x.getLastName(), x.getStatus(), x.getOib())).
+                collect(Collectors.toList());
+        return cardRequests;
+    }
 }
--- card.crud/src/test/java/com/ben/card/crud/controller/CreditCardControllerTest.java
+++ card.crud/src/test/java/com/ben/card/crud/controller/CreditCardControllerTest.java
@@ -1,7 +1,81 @@
 package com.ben.card.crud.controller;
 
+import com.ben.card.crud.repository.NewCreditCardRequestDao;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@TestPropertySource("/application-test.properties")
+@AutoConfigureMockMvc
 @SpringBootTest
+@Transactional
 public class CreditCardControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Value("${sql.script.create.newcreditcard.request}")
+    private String sqlAddCards;
+
+    @Value("${sql.script.delete.newcreditcard.request}")
+    private String sqlDeleteCards;
+
+    @Autowired
+    private NewCreditCardRequestController controller;
+
+    @Autowired
+    private NewCreditCardRequestDao cardRequestDao;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
+
+
+    @BeforeEach
+    public void initializeDatabase() {
+        jdbc.execute(sqlAddCards);
+    }
+
+    // get by oib
+    @Test
+    public void getCreditCardRequestByOibTest() throws Exception {
+        // use get request together with oib to fetch card requests for that client
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/client-cards/{oib}", "11112223334"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        // check if response is ok
+    }
+
+
+
+    // post - new request
+
+    // delete
+
+    @AfterEach
+    public void restoreDatabase() {
+        jdbc.execute(sqlDeleteCards);
+    }
+
 }
